2658e025393b9580ed734d040602f76a1ce931ab
mulan19851955-pixel <mulan19851955@gmail.com>
2026-01-10 19:33:36 +0100

    🥵🥵🥵😮‍💨

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,7 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import ChatScreen from './(tabs)/chat';
 import 'react-native-reanimated';
 
@@ -13,10 +15,15 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Stack>
+  <Stack>
+    {/* Твои текущие экраны */}
+    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    <Stack.Screen name="chat/[id]" options={{ headerShown: false }} />
       
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="index" options={{ headerShown: false }} /> {/* если есть стартовая */}
-    </Stack>
+      <Stack.Screen 
+      name="auth/phoneLogin" 
+      options={{ headerShown: false }} // или true, если нужен заголовок
+    />
+  </Stack>
   );
 }
